5fa0295d810a6ee894275f93184ec25f62af7e13
fumamosss <fumamosss@gmail.com>
2024-02-10 22:55:12 +0500

    govno

---
+++ z4/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    int a = num % 10;
+    printf("%d ", num);
+    printf((num > 4 && num < 21) ? "копеек" : a == 1 ? "копейка" : a > 1 && a < 5 ? "копейки" : "копеек");
+    return 0;
+}
