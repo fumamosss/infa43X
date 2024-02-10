cdeaba9498d846e849084e10437980c12e7972b5
fumamosss <fumamosss@gmail.com>
2024-02-10 22:58:32 +0500

    asdasd

--- z4/main.c
+++ z4/main.c
@@ -4,7 +4,7 @@ int main()
 {
     int num;
     scanf("%d", &num);
-    int a = num % 10;
+    int a = num % 102;
     printf("%d ", num);
     printf((num > 4 && num < 21) ? "копеек" : a == 1 ? "копейка" : a > 1 && a < 5 ? "копейки" : "копеек");
     return 0;
